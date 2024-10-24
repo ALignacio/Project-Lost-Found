26a4da035d2e1d9e67d9c5189e48aaef798af6eb
ALignacio <andrielawrence8@gmail.com>
2024-10-24 20:43:33 +0800

    updated Login and Register Forms

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,10 +1,28 @@
+<script>
+export default {
+  data: () => ({
+    visible: false,
+  }),
+}
+</script>
 <template>
   <v-form fast-fail @submit.prevent>
-    <v-text-field label="Username" variant="outlined"></v-text-field>
     <v-text-field
-      label="Password"
+      density="compact"
+      placeholder="Email address"
+      prepend-inner-icon="mdi-email-outline"
       variant="outlined"
-      type="password"
+      label="Email"
+    ></v-text-field>
+    <v-text-field
+      :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+      :type="visible ? 'text' : 'password'"
+      density="compact"
+      placeholder="Enter your password"
+      prepend-inner-icon="mdi-lock-outline"
+      variant="outlined"
+      @click:append-inner="visible = !visible"
+      label="Password"
     ></v-text-field>
 
     <v-btn class="mt-2 bg-primary" type="submit" block prepend-icon="mdi-login"
